1c390dad340c0965111501e70a8841268bca67e7
ChucktownTiger <clemsonalum07@gmail.com>
2022-05-23 23:03:21 -0400

    added form funciontality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 var formE1 =  document.querySelector("#task-form");
-var taskToDoE1 = document.querySelector("#tasks-to-do");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
-    listItemE1.textContent = "This is a new task.";
-    taskToDoE1.appendChild(listItemE1)
+
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoEl)
+
+    tasksToDoE1.appendChild(listItemE1)
 };
 
 formE1.addEventListener("submit",createTaskHandler);
